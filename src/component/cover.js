7cfe8709f31fc6c228dd8c0163e162ec198e2f7d
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-04-20 21:37:06 -0400

    Rename app.js and create cover page component

---
+++ src/component/cover.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Cover extends Component {
+
+    render() {
+        return (
+            <div id="cover"></div>
+        )
+    }
+}
+
+export default Cover;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './style/index.css';
+import Generator from './Generator';
+import Cover from './component/cover'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Generator />
+    <Cover />
   </React.StrictMode>
 );
