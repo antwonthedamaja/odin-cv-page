7eb0a287d165df97552a737c33ccc72a9b84aa6a
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-04-26 22:27:32 -0400

    Change component to functional with single hook

--- src/Generator.js
+++ src/Generator.js
@@ -1,113 +1,98 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import Cover from './component/Cover';
 import './style/generator.css';
 
-class Generator extends Component {
-  constructor() {
-    super();
+const Generator = () => {
+  const [state, setState] = useState({
+    fname: '',
+    lname: '',
+    title: '',
+    address: '',
+    tel: '',
+    email: '',
+    desc: '',
+    job1: '',
+    company1: '',
+    date1: '',
+    role1: '',
+    jobCount: 1,
+    school1: '',
+    attended1: '',
+    field1: '',
+    schoolCount: 1
+  });
 
-    this.state = {
-      fname: '',
-      lname: '',
-      title: '',
-      address: '',
-      tel: '',
-      email: '',
-      desc: '',
-      job1: '',
-      company1: '',
-      date1: '',
-      role1: '',
-      jobCount: 1,
-      school1: '',
-      attended1: '',
-      field1: '',
-      schoolCount: 1
-    };
-
-    this.cvProps = structuredClone(this.state);
-  }
-
-  handleChange = (e) => {
-    const clone = structuredClone(this.state);
+  function handleChange(e) {
+    const clone = structuredClone(state);
     clone[e.target.name] = e.target.value;
-    this.setState(clone);
+    setState(clone);
   }
 
-  infoSubmit = () => {
-    this.cvProps = structuredClone(this.state);
-    this.forceUpdate();
-  }
-
-  renderJobFields = () => {
-    const array = [];
-    for (let i = 1; i <= this.state.jobCount; i++) {
-      const element = <div className="field">{'Job ' + i}
-      <input type='text' name={'job' + i} placeholder='Job title' onChange={this.handleChange}></input>
-      <input type='text' name={'company' + i} placeholder='Company' onChange={this.handleChange}></input>
-      <input type='text' name={'date' + i} placeholder='Date' onChange={this.handleChange}></input>
-      <textarea rows="3" name={'role' + i} placeholder='Role description' onChange={this.handleChange}></textarea>
-    </div>;
-      array.push(element);
-    }
-    return array
-  }
-
-  addJob = () => {
-    const clone = structuredClone(this.state);
+  function addJob() {
+    const clone = structuredClone(state);
     clone.jobCount += 1;
     clone['job' + clone.jobCount] = '';
     clone['company' + clone.jobCount] = '';
     clone['date' + clone.jobCount] = '';
     clone['role' + clone.jobCount] = '';
-    this.setState(clone);
+    setState(clone);
   }
 
-  renderSchoolFields = () => {
+  function addSchool() {
+    const clone = structuredClone(state);
+    clone.schoolCount += 1;
+    clone['school' + clone.schoolCount] = '';
+    clone['attended' + clone.schoolCount] = '';
+    clone['field' + clone.schoolCount] = '';
+    setState(clone);
+  }
+  
+  function renderJobFields() {
     const array = [];
-    for (let i = 1; i <= this.state.schoolCount; i++) {
+    for (let i = 1; i <= state.jobCount; i++) {
+      const element = <div className="field">{'Job ' + i}
+      <input type='text' name={'job' + i} placeholder='Job title' onChange={handleChange}></input>
+      <input type='text' name={'company' + i} placeholder='Company' onChange={handleChange}></input>
+      <input type='text' name={'date' + i} placeholder='Date' onChange={handleChange}></input>
+      <textarea rows="3" name={'role' + i} placeholder='Role description' onChange={handleChange}></textarea>
+    </div>;
+      array.push(element);
+    }
+    return array
+  }
+  
+  function renderSchoolFields() {
+    const array = [];
+    for (let i = 1; i <= state.schoolCount; i++) {
       const element = <div className="field">{'School ' + i}
-        <input type='text' name={'school' + i} placeholder='School' onChange={this.handleChange}></input>
-        <input type='text' name={'attended' + i} placeholder='Years attended' onChange={this.handleChange}></input>
-        <input type='text' name={'field' + i} placeholder='Primary field' onChange={this.handleChange}></input>
+        <input type='text' name={'school' + i} placeholder='School' onChange={handleChange}></input>
+        <input type='text' name={'attended' + i} placeholder='Years attended' onChange={handleChange}></input>
+        <input type='text' name={'field' + i} placeholder='Primary field' onChange={handleChange}></input>
     </div>;
       array.push(element);
     }
     return array;
   }
 
-  addSchool = () => {
-    const clone = structuredClone(this.state);
-    clone.schoolCount += 1;
-    clone['school' + clone.schoolCount] = '';
-    clone['attended' + clone.schoolCount] = '';
-    clone['field' + clone.schoolCount] = '';
-    this.setState(clone);
-  }
-
-  render() {
-
-    return (
-      <div id="app">
-        <div id='generator'>Personal Info
-          <input type='text' name="fname" placeholder='First Name' onChange={this.handleChange}></input>
-          <input type='text' name="lname" placeholder='Last Name' onChange={this.handleChange}></input>
-          <input type='text' name="title" placeholder='Profession title' onChange={this.handleChange}></input>
-          <input type='text' name="address" placeholder='Street Address' onChange={this.handleChange}></input>
-          <input type='text' name="tel" placeholder='Phone Number' onChange={this.handleChange}></input>
-          <input type='text' name="email" placeholder='Email Address' onChange={this.handleChange}></input>
-          <textarea rows="3" name="desc" placeholder='Description' onChange={this.handleChange}></textarea>
-          <button type='button' onClick={this.infoSubmit}>Update CV</button>
-          {this.renderJobFields()}
-          <button type='button' onClick={this.addJob}>Add Additional Experience</button>
-          {this.renderSchoolFields()}
-          <button type='button' onClick={this.addSchool}>Add Additional Education</button>
-          <div className="spacing">&nbsp;</div>
-        </div>
-        <Cover props={this.cvProps} />
+  return (
+    <div id="app">
+      <div id='generator'>Personal Info
+        <input type='text' name="fname" placeholder='First Name' onChange={handleChange}></input>
+        <input type='text' name="lname" placeholder='Last Name' onChange={handleChange}></input>
+        <input type='text' name="title" placeholder='Profession title' onChange={handleChange}></input>
+        <input type='text' name="address" placeholder='Street Address' onChange={handleChange}></input>
+        <input type='text' name="tel" placeholder='Phone Number' onChange={handleChange}></input>
+        <input type='text' name="email" placeholder='Email Address' onChange={handleChange}></input>
+        <textarea rows="3" id="desc" name="desc" placeholder='Description' onChange={handleChange}></textarea>
+        {renderJobFields()}
+        <button type='button' onClick={addJob}>Add Additional Experience</button>
+        {renderSchoolFields()}
+        <button type='button' onClick={addSchool}>Add Additional Education</button>
+        <div className="spacing">&nbsp;</div>
       </div>
-    );
-  }
+      <Cover props={state} />
+    </div>
+  );
 }
 
 export default Generator;
